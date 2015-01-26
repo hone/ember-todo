b4fd687638e2f940b42e2842217c297d712c3164
Terence Lee <hone02@gmail.com>
2015-01-26 17:46:00 -0600

    allow adding new tasks

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -4,5 +4,20 @@ export default Ember.ArrayController.extend({
   todoCount: Ember.computed.alias("model.length"),
   completedCount: function() {
 	return this.get("model").filterBy("isCompleted").get("length");
-  }.property("@each.isCompleted")
+  }.property("@each.isCompleted"),
+  newTitle: "",
+  actions: {
+	createTodo: function() {
+	  var title = this.get('newTitle');
+	  var todo = this.store.createRecord('todo', {
+		title: title,
+		isCompleted: false
+	  });
+
+	  var controller = this;
+	  todo.save().then(function(todo) {
+		controller.set('newTitle', '');
+	  });
+	}
+  }
 });
