de57dee30f11d0595dbce5f556b478f23d374b2f
Terence Lee <hone02@gmail.com>
2015-01-26 17:03:26 -0600

    dynamically generate number of todos

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+  todoCount: Ember.computed.alias("model.length"),
+  completedCount: function() {
+	return this.get("model").filterBy("isCompleted").get("length");
+  }.property("@each.isCompleted")
 });
