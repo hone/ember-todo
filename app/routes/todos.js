d2f8fc9076f55858013d65142fefefe65945ec81
Terence Lee <hone02@gmail.com>
2015-01-27 14:45:06 -0600

    fix clear completed

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -2,8 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.find('todo').then(function(todos) {
-      return todos.filterBy('isNew', false);
-    });
+    return this.store.find('todo');
+    //return this.store.find('todo').then(function(todos) {
+    //  return todos.filterBy('isNew', false);
+    //});
   }
 });
