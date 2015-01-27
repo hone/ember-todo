a3987d9695cdc1eb93fefb43ce0aee7561dab959
Terence Lee <hone02@gmail.com>
2015-01-27 15:19:45 -0600

    add active and completed views

--- app/router.js
+++ app/router.js
@@ -7,6 +7,8 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.resource('todos', { path: '/' }, function() {
+    this.route('active');
+    this.route('completed');
   });
 });
 
