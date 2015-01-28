a1e0c4e077541a5a99a47551ef2fd4b699daecb3
Terence Lee <hone02@gmail.com>
2015-01-28 12:58:41 -0600

    whitespace

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -3,41 +3,41 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
   todoCount: Ember.computed.alias("model.length"),
   completedTodos: function() {
-	return this.get("model").filterBy("isCompleted");
+    return this.get("model").filterBy("isCompleted");
   }.property("@each.isCompleted"),
   completedCount: function() {
-	return this.get("completedTodos").get("length");
+    return this.get("completedTodos").get("length");
   }.property("@each.isCompleted"),
   todo: function() {
-	return this.store.createRecord('todo', {
-	  isCompleted: false
-	});
+    return this.store.createRecord('todo', {
+      isCompleted: false
+    });
   }.property(),
   allCompleted: function(key, value) {
-	if (value === undefined) {
-	  return this.get("completedCount") == this.get("model").get("length");
-	} else {
-	  this.get("model").forEach(function(todo) {
-		todo.set("isCompleted", value);
-		todo.save();
-	  });
-	  return value;
-	}
+    if (value === undefined) {
+      return this.get("completedCount") == this.get("model").get("length");
+    } else {
+      this.get("model").forEach(function(todo) {
+        todo.set("isCompleted", value);
+        todo.save();
+      });
+      return value;
+    }
   }.property("@each.isCompleted"),
 
   actions: {
-	createTodo: function() {
-	  var controller = this;
-	  this.get('todo').save().then(function() {
-		controller.set('todo', controller.store.createRecord('todo', {
-		  isCompleted: false
-		}));
-	  });
-	},
-	clearCompleted: function() {
-	  this.get("completedTodos").forEach(function(item) {
-		item.destroyRecord();
-	  });
-	}
+    createTodo: function() {
+      var controller = this;
+      this.get('todo').save().then(function() {
+        controller.set('todo', controller.store.createRecord('todo', {
+          isCompleted: false
+        }));
+      });
+    },
+    clearCompleted: function() {
+      this.get("completedTodos").forEach(function(item) {
+        item.destroyRecord();
+      });
+    }
   }
 });
