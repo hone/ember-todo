56a90a1fc7998f4c5738e71a4465360d28a1cb15
Terence Lee <hone02@gmail.com>
2015-03-10 16:52:52 -0500

    remove deprecations

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.ObjectController.extend({
+export default Ember.Controller.extend({
   isCompleted: function(key, value) {
 	var model = this.get('model');
 
