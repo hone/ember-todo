b5a2b7f2dddc0f59d18ed8d8969334b61b21ce8f
Terence Lee <hone02@gmail.com>
2015-03-14 15:39:59 -0500

    don't display new objects in the view

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.find('todo');
+    this.store.findAll('todo');
+
+    return this.store.filter('todo', function(todo) {
+      return !todo.get('isNew');
+    });
   }
 });
