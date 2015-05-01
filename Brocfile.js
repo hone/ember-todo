fadb42a9372811c88cf517ed087ea0ee513e360b
Terence Lee <hone02@gmail.com>
2015-05-01 10:20:01 -0500

    update ember-cli-fastboot

--- Brocfile.js
+++ Brocfile.js
@@ -4,7 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
   fingerprint: {
-    enabled: false
+    enabled: true
   },
   storeConfigInMeta: false
 });
