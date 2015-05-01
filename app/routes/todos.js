4d4779c90800a05faf9b0ad9f3bad396ec91bf0b
Terence Lee <hone02@gmail.com>
2015-05-01 10:20:24 -0500

    @tomdale's fault

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -2,10 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    this.store.findAll('todo');
+    var routeContext = this;
 
-    return this.store.filter('todo', function(todo) {
-      return !todo.get('isNew');
-    });
+    return this.store.findAll('todo')
+      .then(function() {
+        return routeContext.store.filter('todo', function(todo) {
+          return !todo.get('isNew');
+        });
+      });
   }
 });
